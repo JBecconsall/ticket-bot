686329e160360ad9b7a89a7299740483779f533d
JBecconsall <joshuabecconsall05@gmail.com>
2024-10-10 02:47:33 +0100

    create ticket on button click

---
+++ src/commands/ticketPanel.js
@@ -0,0 +1,53 @@
+const {
+    ButtonBuilder,
+    EmbedBuilder,
+    MessagePayload,
+    ActionRowBuilder,
+    ButtonStyle
+} = require("discord.js")
+const {
+    CommandType
+} = require("wokcommands");
+module.exports = {
+    category: 'Tickets',
+    name: 'ticketpanel',
+    description: 'Send panel for ticket selection',
+    testOnly: true,
+    type: CommandType.SLASH,
+    ownerOnly: true,
+
+    callback: async ({
+        interaction,
+        client
+    }) => {
+
+        const gensupButton = new ButtonBuilder()
+            .setCustomId('gensup')
+            .setEmoji('📩')
+            .setLabel('General Support')
+            .setStyle(ButtonStyle.Secondary)
+
+        const row1 = new ActionRowBuilder()
+            .addComponents(gensupButton)
+
+
+        const embed = new EmbedBuilder()
+            .setColor('Blue')
+            .setTitle('Ticket Categories')
+            .setThumbnail(interaction.guild.iconURL())
+            .setDescription('>>> 🌟 Hey there! Having some trouble? \nOur support tickets are the best way to get in contact with the support team. A support ticket can be opened for any kind of issue. \n\n⏰ Our support team members handle tickets as quickly as they can. Unless it\'s urgent, please don\'t ping staff.')
+            .setFooter({
+                text: 'Ticket System'
+            })
+            .setTimestamp()
+
+
+        let msg = MessagePayload.create(interaction.channel, {
+            embeds: [embed],
+            components: [row1]
+        })
+
+        await interaction.channel.send(msg)
+
+    }
+}
---
+++ src/events/ticketCreate.js
@@ -0,0 +1,81 @@
+const {
+    EmbedBuilder,
+    ChannelType,
+    PermissionsBitField,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+    MessagePayload
+} = require("discord.js");
+const {client} = require("../index");
+const config = require('../utils/config.json');
+module.exports = async (instance, message) => {
+
+    
+    client.on('interactionCreate', async (interaction) => {
+
+        const genSupOpened = new EmbedBuilder()
+        .setColor('Aqua')
+        .setTitle('General Support Ticket')
+        .setDescription('>>> Thank you for contacting the support team. \nPlease tell us how we can help and await a response!')
+        .setTimestamp()
+        .setThumbnail(interaction.guild.iconURL())
+        .setFooter({
+            text: 'Ticket System'
+        })
+
+        if (!interaction.isButton()) return;
+
+        // ?GENERAL SUPPORT 
+        if (interaction.customId === 'gensup') {
+
+            let channelName = `support-${interaction.user.tag}`
+            let parent = config.TICKET_CATEGORY
+
+            let newTicket = await interaction.guild.channels.create({
+                name: channelName,
+                parent: parent,
+                topic: `${interaction.user.id}`,
+                type: ChannelType.GuildText,
+                permissionOverwrites: [{
+                        id: interaction.user.id,
+                        allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.ReadMessageHistory, PermissionsBitField.Flags.EmbedLinks, PermissionsBitField.Flags.AttachFiles, PermissionsBitField.Flags.SendMessages]
+                    },
+                    {
+                        id: config.STAFF_ID,
+                        allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.ReadMessageHistory, PermissionsBitField.Flags.EmbedLinks, PermissionsBitField.Flags.AttachFiles, PermissionsBitField.Flags.SendMessages]
+                    },
+                    {
+                        id: interaction.guild.roles.everyone,
+                        deny: [PermissionsBitField.Flags.ViewChannel]
+                    }
+                ],
+
+            }).then(async channel => {
+                const closeButton = new ButtonBuilder()
+                    .setCustomId('close')
+                    .setEmoji('🔒')
+                    .setLabel('Close')
+                    .setStyle(ButtonStyle.Danger)
+
+                const claimButton = new ButtonBuilder()
+                    .setCustomId('claim')
+                    .setEmoji('🤚🏻')
+                    .setLabel('Claim')
+                    .setStyle(ButtonStyle.Primary)
+
+                const row1 = new ActionRowBuilder()
+                    .addComponents(closeButton, claimButton)
+
+                await interaction.reply({
+                    content: `Your ticket has been made at <#${channel.id}>`,
+                    ephemeral: true
+                })
+
+                await channel.send({embeds: [genSupOpened], components: [row1]})
+            })
+
+        } // *END
+
+    })
+}
